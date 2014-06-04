dfca26eff343540b80e668ee13308245eecfcf70
Dominik Pesch <d.pesch@11com7.de>
2014-06-04 22:16:32 +0200

    use RuntimeException instead of UnsupportedOperationException to catch the IllegalStateException and UnsupportedOperationException without doubling (in fact multiplying) the code

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -735,11 +735,11 @@ private String ouputModifiedBitmap(Bitmap bitmap, Uri uri) throws IOException {
         Uri uri;
         try {
             uri = this.cordova.getActivity().getContentResolver().insert(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-        } catch (UnsupportedOperationException e) {
+        } catch (RuntimeException e) {
             LOG.d(LOG_TAG, "Can't write to external media storage.");
             try {
                 uri = this.cordova.getActivity().getContentResolver().insert(android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI, values);
-            } catch (UnsupportedOperationException ex) {
+            } catch (RuntimeException ex) {
                 LOG.d(LOG_TAG, "Can't write to internal media storage.");
                 return null;
             }
