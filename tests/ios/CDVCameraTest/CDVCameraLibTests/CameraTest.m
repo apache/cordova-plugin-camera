17790f6f954641f5694006e80f573107dc20a238
Shazron Abdullah <shazron@gmail.com>
2014-12-19 13:18:06 -0500

    Completed tests for CDVCameraPicker and CDVPictureOptions.

--- tests/ios/CDVCameraTest/CDVCameraLibTests/CameraTest.m
+++ tests/ios/CDVCameraTest/CDVCameraLibTests/CameraTest.m
@@ -24,6 +24,7 @@
 #import <Cordova/NSArray+Comparisons.h>
 #import <Cordova/NSData+Base64.h>
 #import <Cordova/NSDictionary+Extensions.h>
+#import <MobileCoreServices/UTCoreTypes.h>
 
 
 @interface CameraTest : XCTestCase
@@ -58,9 +59,13 @@
 
 - (void) testPictureOptionsCreate
 {
+    NSArray* args;
+    CDVPictureOptions* options;
+    NSDictionary* popoverOptions;
+    
     // No arguments, check whether the defaults are set
-    NSArray* args = @[];
-    CDVPictureOptions* options = [CDVPictureOptions createFromTakePictureArguments:args];
+    args = @[];
+    options = [CDVPictureOptions createFromTakePictureArguments:args];
     
     XCTAssertEqual(options.quality, @(50));
     XCTAssertEqual(options.destinationType, (int)DestinationTypeFileUri);
@@ -77,15 +82,133 @@
     XCTAssertEqual(options.popoverSupported, NO);
     XCTAssertEqual(options.usesGeolocation, NO);
     
+    // Set each argument, check whether they are set. different from defaults
+    popoverOptions = @{ @"x" : @1, @"y" : @2, @"width" : @3, @"height" : @4 };
     
-    // Set each argument, check whether they are set
-    // TODO:
+    args = @[
+             @(49),
+             @(DestinationTypeDataUrl),
+             @(UIImagePickerControllerSourceTypePhotoLibrary),
+             @(120),
+             @(240),
+             @(EncodingTypePNG),
+             @(MediaTypeVideo),
+             @YES,
+             @YES,
+             @YES,
+             popoverOptions,
+             @(UIImagePickerControllerCameraDeviceFront),
+             ];
+    options = [CDVPictureOptions createFromTakePictureArguments:args];
     
-    
+    XCTAssertEqual(options.quality, @(49));
+    XCTAssertEqual(options.destinationType, (int)DestinationTypeDataUrl);
+    XCTAssertEqual(options.sourceType, (int)UIImagePickerControllerSourceTypePhotoLibrary);
+    XCTAssertEqual(options.targetSize.width, 120);
+    XCTAssertEqual(options.targetSize.height, 240);
+    XCTAssertEqual(options.encodingType, (int)EncodingTypePNG);
+    XCTAssertEqual(options.mediaType, (int)MediaTypeVideo);
+    XCTAssertEqual(options.allowsEditing, YES);
+    XCTAssertEqual(options.correctOrientation, YES);
+    XCTAssertEqual(options.saveToPhotoAlbum, YES);
+    XCTAssertEqualObjects(options.popoverOptions, popoverOptions);
+    XCTAssertEqual(options.cameraDirection, (int)UIImagePickerControllerCameraDeviceFront);
+    XCTAssertEqual(options.popoverSupported, NO);
+    XCTAssertEqual(options.usesGeolocation, NO);
 }
 
 - (void) testCameraPickerCreate
 {
+    NSDictionary* popoverOptions;
+    NSArray* args;
+    CDVPictureOptions* pictureOptions;
+    CDVCameraPicker* picker;
+    
+    // Souce is Camera, and image type
+    
+    popoverOptions = @{ @"x" : @1, @"y" : @2, @"width" : @3, @"height" : @4 };
+    args = @[
+             @(49),
+             @(DestinationTypeDataUrl),
+             @(UIImagePickerControllerSourceTypeCamera),
+             @(120),
+             @(240),
+             @(EncodingTypePNG),
+             @(MediaTypeAll),
+             @YES,
+             @YES,
+             @YES,
+             popoverOptions,
+             @(UIImagePickerControllerCameraDeviceFront),
+             ];
+    pictureOptions = [CDVPictureOptions createFromTakePictureArguments:args];
+    
+    if ([UIImagePickerController isSourceTypeAvailable:pictureOptions.sourceType]) {
+        picker = [CDVCameraPicker createFromPictureOptions:pictureOptions];
+        
+        XCTAssertEqualObjects(picker.pictureOptions, pictureOptions);
+
+        XCTAssertEqual(picker.sourceType, pictureOptions.sourceType);
+        XCTAssertEqual(picker.allowsEditing, pictureOptions.allowsEditing);
+        XCTAssertEqualObjects(picker.mediaTypes, @[(NSString*)kUTTypeImage]);
+        XCTAssertEqual(picker.cameraDevice, pictureOptions.cameraDirection);
+    }
+
+    // Souce is not Camera, and all media types
+
+    args = @[
+          @(49),
+          @(DestinationTypeDataUrl),
+          @(UIImagePickerControllerSourceTypePhotoLibrary),
+          @(120),
+          @(240),
+          @(EncodingTypePNG),
+          @(MediaTypeAll),
+          @YES,
+          @YES,
+          @YES,
+          popoverOptions,
+          @(UIImagePickerControllerCameraDeviceFront),
+          ];
+    pictureOptions = [CDVPictureOptions createFromTakePictureArguments:args];
+
+    if ([UIImagePickerController isSourceTypeAvailable:pictureOptions.sourceType]) {
+        picker = [CDVCameraPicker createFromPictureOptions:pictureOptions];
+
+         XCTAssertEqualObjects(picker.pictureOptions, pictureOptions);
+         
+         XCTAssertEqual(picker.sourceType, pictureOptions.sourceType);
+         XCTAssertEqual(picker.allowsEditing, pictureOptions.allowsEditing);
+         XCTAssertEqualObjects(picker.mediaTypes, [UIImagePickerController availableMediaTypesForSourceType:picker.sourceType]);
+    }
+    
+    // Souce is not Camera, and either Image or Movie media type
+
+    args = @[
+             @(49),
+             @(DestinationTypeDataUrl),
+             @(UIImagePickerControllerSourceTypePhotoLibrary),
+             @(120),
+             @(240),
+             @(EncodingTypePNG),
+             @(MediaTypeVideo),
+             @YES,
+             @YES,
+             @YES,
+             popoverOptions,
+             @(UIImagePickerControllerCameraDeviceFront),
+             ];
+    pictureOptions = [CDVPictureOptions createFromTakePictureArguments:args];
+    
+    if ([UIImagePickerController isSourceTypeAvailable:pictureOptions.sourceType]) {
+        picker = [CDVCameraPicker createFromPictureOptions:pictureOptions];
+        
+        XCTAssertEqualObjects(picker.pictureOptions, pictureOptions);
+        
+        XCTAssertEqual(picker.sourceType, pictureOptions.sourceType);
+        XCTAssertEqual(picker.allowsEditing, pictureOptions.allowsEditing);
+        XCTAssertEqualObjects(picker.mediaTypes, @[(NSString*)kUTTypeMovie]);
+    }
 }
 
 - (void) testImageScaleCropForSize {
