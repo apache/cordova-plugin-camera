2df903ae611e1961968e85f75d0fe56c9ee11371
Serge Huijben <thehuijb@gmail.com>
2015-02-20 00:32:31 +0100

    more distinguishable error messages enable dropbox images (won't crash the app anymore) option to choose only from gallery apps, to stop people from introducing new problems

--- src/android/FileHelper.java
+++ src/android/FileHelper.java
@@ -18,8 +18,14 @@
  */
 package org.apache.cordova.camera;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.CursorLoader;
 import android.database.Cursor;
 import android.net.Uri;
+import android.os.Build;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.webkit.MimeTypeMap;
 
 import org.apache.cordova.CordovaInterface;
@@ -35,85 +41,201 @@ public class FileHelper {
     private static final String _DATA = "_data";
 
     /**
-     * Returns the real path of the given URI string.
-     * If the given URI string represents a content:// URI, the real path is retrieved from the media store.
+     * Returns the real path of the given URI string. If the given URI string
+     * represents a content:// URI, the real path is retrieved from the media
+     * store.
      *
-     * @param uriString the URI string of the audio/image/video
-     * @param cordova the current application context
+     * @param uriString
+     *            the URI string of the audio/image/video
+     * @param cordova
+     *            the current application context
      * @return the full path to the file
      */
     @SuppressWarnings("deprecation")
-    public static String getRealPath(String uriString, CordovaInterface cordova) {
+    public static String getRealPath(Uri uri, CordovaInterface cordova) {
         String realPath = null;
 
-        if (uriString.startsWith("content://")) {
-            String[] proj = { _DATA };
-            Cursor cursor = cordova.getActivity().managedQuery(Uri.parse(uriString), proj, null, null, null);
-            int column_index = cursor.getColumnIndexOrThrow(_DATA);
-            cursor.moveToFirst();
-            realPath = cursor.getString(column_index);
-            if (realPath == null) {
-                LOG.e(LOG_TAG, "Could get real path for URI string %s", uriString);
-            }
-        } else if (uriString.startsWith("file://")) {
-            realPath = uriString.substring(7);
-            if (realPath.startsWith("/android_asset/")) {
-                LOG.e(LOG_TAG, "Cannot get real path for URI string %s because it is a file:///android_asset/ URI.", uriString);
-                realPath = null;
-            }
-        } else {
-            realPath = uriString;
-        }
+        if (Build.VERSION.SDK_INT < 11)
+            realPath = FileHelper.getRealPathFromURI_BelowAPI11(
+                    cordova.getActivity(), uri);
+
+        // SDK >= 11 && SDK < 19
+        else if (Build.VERSION.SDK_INT < 19)
+            realPath = FileHelper.getRealPathFromURI_API11to18(
+                    cordova.getActivity(), uri);
+
+        // SDK > 19 (Android 4.4)
+        else
+            realPath = FileHelper.getRealPathFromURI_API19(
+                    cordova.getActivity(), uri);
+
+        // if (uriString.startsWith("content://")) {
+        // String[] proj = { _DATA };
+        // Cursor cursor =
+        // cordova.getActivity().managedQuery(Uri.parse(uriString), proj, null,
+        // null, null);
+        // int column_index = cursor.getColumnIndexOrThrow(_DATA);
+        // cursor.moveToFirst();
+        // realPath = cursor.getString(column_index);
+        // if (realPath == null) {
+        // LOG.e(LOG_TAG, "Could get real path for URI string %s", uriString);
+        // }
+        // } else if (uriString.startsWith("file://")) {
+        // realPath = uriString.substring(7);
+        // if (realPath.startsWith("/android_asset/")) {
+        // LOG.e(LOG_TAG,
+        // "Cannot get real path for URI string %s because it is a file:///android_asset/ URI.",
+        // uriString);
+        // realPath = null;
+        // }
+        // } else {
+        // realPath = uriString;
+        // }
 
         return realPath;
     }
 
     /**
-     * Returns the real path of the given URI.
-     * If the given URI is a content:// URI, the real path is retrieved from the media store.
+     * Returns the real path of the given URI. If the given URI is a content://
+     * URI, the real path is retrieved from the media store.
      *
-     * @param uri the URI of the audio/image/video
-     * @param cordova the current application context
+     * @param uri
+     *            the URI of the audio/image/video
+     * @param cordova
+     *            the current application context
      * @return the full path to the file
      */
-    public static String getRealPath(Uri uri, CordovaInterface cordova) {
-        return FileHelper.getRealPath(uri.toString(), cordova);
+    public static String getRealPath(String uriString, CordovaInterface cordova) {
+        return FileHelper.getRealPath(Uri.parse(uriString), cordova);
     }
 
+    @SuppressLint("NewApi")
+    public static String getRealPathFromURI_API19(Context context, Uri uri) {
+        String filePath = "";
+        try {
+            String wholeID = DocumentsContract.getDocumentId(uri);
+
+            // Split at colon, use second item in the array
+            String id = wholeID.indexOf(":") > -1 ? wholeID.split(":")[1]
+                    : wholeID.indexOf(";") > -1 ? wholeID.split(";")[1]
+                            : wholeID;
+
+            String[] column = { MediaStore.Images.Media.DATA };
+
+            // where id is equal to
+            String sel = MediaStore.Images.Media._ID + "=?";
+
+            Cursor cursor = context.getContentResolver().query(
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI, column, sel,
+                    new String[] { id }, null);
+
+            int columnIndex = cursor.getColumnIndex(column[0]);
+
+            if (cursor.moveToFirst()) {
+                filePath = cursor.getString(columnIndex);
+            }
+            cursor.close();
+        } catch (Exception e) {
+            filePath = "";
+        }
+        return filePath;
+    }
+
+    @SuppressLint("NewApi")
+    public static String getRealPathFromURI_API11to18(Context context,
+            Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        String result = null;
+
+        try {
+            CursorLoader cursorLoader = new CursorLoader(context, contentUri,
+                    proj, null, null, null);
+            Cursor cursor = cursorLoader.loadInBackground();
+
+            if (cursor != null) {
+                int column_index = cursor
+                        .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+                cursor.moveToFirst();
+                result = cursor.getString(column_index);
+            }
+        } catch (Exception e) {
+            result = null;
+        }
+        return result;
+    }
+
+    public static String getRealPathFromURI_BelowAPI11(Context context,
+            Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        String result = null;
+
+        try {
+            Cursor cursor = context.getContentResolver().query(contentUri,
+                    proj, null, null, null);
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            result = cursor.getString(column_index);
+
+        } catch (Exception e) {
+            result = null;
+        }
+        return result;
+    }
     /**
      * Returns an input stream based on given URI string.
      *
-     * @param uriString the URI string from which to obtain the input stream
-     * @param cordova the current application context
-     * @return an input stream into the data at the given URI or null if given an invalid URI string
+     * @param uriString
+     *            the URI string from which to obtain the input stream
+     * @param cordova
+     *            the current application context
+     * @return an input stream into the data at the given URI or null if given
+     *         an invalid URI string
      * @throws IOException
      */
-    public static InputStream getInputStreamFromUriString(String uriString, CordovaInterface cordova) throws IOException {
+    public static InputStream getInputStreamFromUriString(String uriString,
+            CordovaInterface cordova) throws IOException {
+        InputStream returnValue = null;
         if (uriString.startsWith("content")) {
             Uri uri = Uri.parse(uriString);
-            return cordova.getActivity().getContentResolver().openInputStream(uri);
+            returnValue = cordova.getActivity().getContentResolver()
+                    .openInputStream(uri);
         } else if (uriString.startsWith("file://")) {
             int question = uriString.indexOf("?");
             if (question > -1) {
-            	uriString = uriString.substring(0,question);
+                uriString = uriString.substring(0, question);
             }
             if (uriString.startsWith("file:///android_asset/")) {
                 Uri uri = Uri.parse(uriString);
                 String relativePath = uri.getPath().substring(15);
-                return cordova.getActivity().getAssets().open(relativePath);
+                returnValue = cordova.getActivity().getAssets()
+                        .open(relativePath);
             } else {
-                return new FileInputStream(getRealPath(uriString, cordova));
+                // might still be content so try that first
+                try {
+                    returnValue = cordova.getActivity().getContentResolver()
+                            .openInputStream(Uri.parse(uriString));
+                } catch (Exception e) {
+                    returnValue = null;
+                }
+                if (returnValue == null) {
+                    returnValue = new FileInputStream(getRealPath(uriString,
+                            cordova));
+                }
             }
         } else {
-            return new FileInputStream(getRealPath(uriString, cordova));
+            returnValue = new FileInputStream(uriString);
         }
+        return returnValue;
     }
 
     /**
-     * Removes the "file://" prefix from the given URI string, if applicable.
-     * If the given URI string doesn't have a "file://" prefix, it is returned unchanged.
+     * Removes the "file://" prefix from the given URI string, if applicable. If
+     * the given URI string doesn't have a "file://" prefix, it is returned
+     * unchanged.
      *
-     * @param uriString the URI string to operate on
+     * @param uriString
+     *            the URI string to operate on
      * @return a path without the "file://" prefix
      */
     public static String stripFileProtocol(String uriString) {
@@ -129,18 +251,20 @@ public class FileHelper {
         if (lastDot != -1) {
             extension = extension.substring(lastDot + 1);
         }
-        // Convert the URI string to lower case to ensure compatibility with MimeTypeMap (see CB-2185).
+        // Convert the URI string to lower case to ensure compatibility with
+        // MimeTypeMap (see CB-2185).
         extension = extension.toLowerCase(Locale.getDefault());
         if (extension.equals("3ga")) {
             return "audio/3gpp";
         }
         return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
     }
-    
+
     /**
      * Returns the mime type of the data specified by the given URI string.
      *
-     * @param uriString the URI string of the data
+     * @param uriString
+     *            the URI string of the data
      * @return the mime type of the specified data
      */
     public static String getMimeType(String uriString, CordovaInterface cordova) {
