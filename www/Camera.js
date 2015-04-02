12895194bb6a3ee75072adc88899c4002c17a69e
Serge Huijben <s.huijben@gmail.com>
2015-04-02 11:26:08 +0200

    correct spelling of callback and return no-result

--- www/Camera.js
+++ www/Camera.js
@@ -72,7 +72,7 @@ cameraExport.cleanup = function(successCallback, errorCallback) {
     exec(successCallback, errorCallback, "Camera", "cleanup", []);
 };
 
-cameraExport.checkForSavedResult = function(successCallBack, errorCallback) {
+cameraExport.checkForSavedResult = function(successCallback, errorCallback) {
     exec(successCallback, errorCallback, "Camera", "checkForSavedResult", []);
 };
 
