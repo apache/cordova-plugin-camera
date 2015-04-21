ac1c161ed37fa87e0fc8d96c2dd223868e497de5
Serge Huijben <s.huijben@gmail.com>
2015-04-21 15:44:42 +0200

    reset only getPictureFromImage

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -1040,7 +1040,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
      */
     public void returnResult(String result){
         this.callbackContext.success(result);
-        this.imageUri = null;
+        //this.imageUri = null;
         this.getPictureFromGallery = false;
     }
 
