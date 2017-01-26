b23dac5afdbc3a40d387ce077af546ad33e0b84d
Sergey Zolotarev <sryze@yandex.com>
2017-01-27 01:41:56 +0600

    CB-12368: Fix permission check on Android
    The plugin was checking whether camera permission was granted but then
    actually requested permission for external storage.
    
    Surprisingly enough this fixed CB-12368.

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -185,7 +185,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
                 }
                 else if ((this.srcType == PHOTOLIBRARY) || (this.srcType == SAVEDPHOTOALBUM)) {
                     // FIXME: Stop always requesting the permission
-                    if(!PermissionHelper.hasPermission(this, permissions[0])) {
+                    if(!PermissionHelper.hasPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
                         PermissionHelper.requestPermission(this, SAVE_TO_ALBUM_SEC, Manifest.permission.READ_EXTERNAL_STORAGE);
                     } else {
                         this.getImage(this.srcType, destType, encodingType);
