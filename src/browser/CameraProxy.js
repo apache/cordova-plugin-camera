765ffe320d7fe6a31ad84e0f52c3b8174d39f72d
Charles Verge <charles.v@nssearch.com>
2018-07-13 21:24:06 -0300

    CB-12873: (browser) Add customization options for capture button, resizing of video on browser resize

--- src/browser/CameraProxy.js
+++ src/browser/CameraProxy.js
@@ -31,6 +31,7 @@ function takePicture (success, error, opts) {
         input.className = 'cordova-camera-select';
         input.type = 'file';
         input.name = 'files[]';
+        input.style.display = 'none';
 
         input.onchange = function (inputEvent) {
             var reader = new FileReader(); /* eslint no-undef : 0 */
@@ -46,6 +47,7 @@ function takePicture (success, error, opts) {
         };
 
         document.body.appendChild(input);
+        input.click();
     }
 }
 
@@ -57,20 +59,134 @@ function capture (success, errorCallback, opts) {
     targetWidth = targetWidth === -1 ? 320 : targetWidth;
     targetHeight = targetHeight === -1 ? 240 : targetHeight;
 
+    // Only assign styles if options does not have any set.
+    if (typeof opts[10] !== 'object' || !opts[10]) {
+        opts[10] = {};
+    }
+
+    if (typeof opts[10].audio === 'undefined') {
+        opts[10].audio = false;
+    }
+
     var video = document.createElement('video');
-    var button = document.createElement('button');
     var parent = document.createElement('div');
-    parent.style.position = 'relative';
-    parent.style.zIndex = HIGHEST_POSSIBLE_Z_INDEX;
-    parent.className = 'cordova-camera-capture';
-    parent.appendChild(video);
-    parent.appendChild(button);
+
+    parent.className = opts[10].className ? opts[10].className : 'cordova-camera-capture';
+    // No custom class, setting default to white background with a video preview and capture.
+    if (!opts[10].className) {
+        parent.className = 'cordova-camera-capture';
+        parent.style.position = 'absolute';
+        parent.style.zIndex = HIGHEST_POSSIBLE_Z_INDEX;
+        parent.style.height = '100%';
+        parent.style.width = '100%';
+        parent.style.top = 0;
+        parent.style.left = 0;
+        parent.style.background = 'white';
+    }
+
+    var videoparent = document.createElement('div');
+    videoparent.className = 'cordova-camera-capture-content';
+    if (opts[10].className) {
+        videoparent.className = opts[10].className + '-content';
+    }
+    videoparent.appendChild(video);
+    parent.appendChild(videoparent);
 
     video.width = targetWidth;
     video.height = targetHeight;
-    button.innerHTML = 'Capture!';
 
-    button.onclick = function () {
+    var buttons = document.createElement('div');
+    if (opts[10].className) {
+        buttons.className = opts[10].className + '-buttons';
+    }
+
+    // If no button is supplied create one.
+    var capturebutton = opts[10].capturebutton ? opts[10].capturebutton : document.createElement('button');
+    if (!opts[10].capturebutton) {
+        capturebutton.type = 'button';
+        capturebutton.innerHTML = 'Capture!';
+    }
+
+    var cancelbutton = opts[10].cancelbutton ? opts[10].cancelbutton : document.createElement('button');
+    if (!opts[10].cancelbutton) {
+        cancelbutton.type = 'button';
+        cancelbutton.innerHTML = 'Cancel';
+    }
+
+    var captureholder = document.createElement('div');
+    if (opts[10].className) {
+        captureholder.className = opts[10].className + '-button';
+    } else {
+        captureholder.style.display = 'inline-block';
+        captureholder.style.width = '49%';
+        captureholder.style.padding = '4px';
+        captureholder.style.textAlign = 'center';
+    }
+    captureholder.appendChild(capturebutton);
+    buttons.appendChild(captureholder);
+
+    var cancelholder = document.createElement('div');
+
+    if (opts[10].className) {
+        cancelholder.className = opts[10].className + '-button';
+    } else {
+        cancelholder.style.display = 'inline-block';
+        cancelholder.style.width = '49%';
+        cancelholder.style.padding = '4px';
+        cancelholder.style.textAlign = 'center';
+    }
+    cancelholder.appendChild(cancelbutton);
+    buttons.appendChild(cancelholder);
+
+    parent.appendChild(buttons);
+
+    // If hover is overlaying other elements prevent clicks from propagating.
+    parent.onclick = function (e) {
+        e.stopPropagation();
+    };
+
+    /**
+     * Resize hover.
+     */
+    var resize = function () {
+        var buttonsheight = buttons.getBoundingClientRect().height;
+        var screenheight = window.innerHeight;
+        var maxheight = screenheight - buttonsheight - 22;
+
+        if (maxheight < 100) {
+            maxheight = 100;
+        }
+
+        video.style.width = '100%';
+        video.style.height = 'auto';
+
+        video.style.maxHeight = maxheight + 'px';
+        video.className = 'video1';
+    };
+
+    /**
+     * Stop video, remove event handlers and element nodes.
+     */
+    var cleanup = function () {
+        // Stop video stream, remove video and button.
+        // Note that MediaStream.stop() is deprecated as of Chrome 47.
+        if (localMediaStream.stop) {
+            localMediaStream.stop();
+        } else {
+            localMediaStream.getTracks().forEach(function (track) {
+                track.stop();
+            });
+        }
+
+        parent.innerHTML = '';
+        parent.parentNode.removeChild(parent);
+        window.removeEventListener('orientationchange', resize);
+        window.removeEventListener('resize', resize);
+    };
+
+    cancelbutton.onclick = cleanup;
+
+    capturebutton.onclick = function () {
         // create a canvas and capture a frame from video stream
         var canvas = document.createElement('canvas');
         canvas.width = targetWidth;
@@ -109,10 +225,14 @@ function capture (success, errorCallback, opts) {
         }
         video.play();
         document.body.appendChild(parent);
+
+        resize();
+        window.addEventListener('orientationchange', resize);
+        window.addEventListener('resize', resize);
     };
 
     if (navigator.getUserMedia) {
-        navigator.getUserMedia({video: true, audio: false}, successCallback, errorCallback);
+        navigator.getUserMedia({video: true, audio: opts[10].audio}, successCallback, errorCallback);
     } else {
         alert('Browser does not support camera :(');
     }
