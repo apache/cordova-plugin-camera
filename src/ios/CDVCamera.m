a9436b1a185b37269366ec3e2c5f337fb14cba61
klodha <klodha@users.noreply.github.com>
2019-09-16 15:10:54 +0530

    ImagePicker returning same image (#306)
    ImagePicker returning same image
    
    Co-authored-by: Jan Piotrowski <piotrowski+github@gmail.com>
    Co-authored-by: Tim Brust <github@timbrust.de>

--- src/ios/CDVCamera.m
+++ src/ios/CDVCamera.m
@@ -398,11 +398,12 @@ static NSString* toBase64(NSData* data) {
     NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
     NSFileManager* fileMgr = [[NSFileManager alloc] init]; // recommended by Apple (vs [NSFileManager defaultManager]) to be threadsafe
     NSString* filePath;
-
-    // generate unique file name
-    int i = 1;
+    
+    // unique file name
+    NSTimeInterval timeStamp = [[NSDate date] timeIntervalSince1970];
+    NSNumber *timeStampObj = [NSNumber numberWithDouble: timeStamp];
     do {
-        filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, CDV_PHOTO_PREFIX, i++, extension];
+        filePath = [NSString stringWithFormat:@"%@/%@%ld.%@", docsPath, CDV_PHOTO_PREFIX, [timeStampObj longValue], extension];
     } while ([fileMgr fileExistsAtPath:filePath]);
 
     return filePath;
