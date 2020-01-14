4f0a8159ee094bf40ad81975a090fb24903628bf
Guillem Martinez <guillem.martinezcanovas@soprasteria.com>
2020-01-14 11:30:42 +0100

    fix(ios): handle cancel video transcoding exception

--- src/ios/CDVCamera.m
+++ src/ios/CDVCamera.m
@@ -512,17 +512,21 @@ static NSString* toBase64(NSData* data) {
 
 - (CDVPluginResult*)resultForVideo:(NSDictionary*)info
 {
-    NSString* moviePath = [[info objectForKey:UIImagePickerControllerMediaURL] path];
+    @try {
+        NSString* moviePath = [[info objectForKey:UIImagePickerControllerMediaURL] path];
 
-    NSArray* spliteArray = [moviePath componentsSeparatedByString: @"/"];
-    NSString* lastString = [spliteArray lastObject];
-    NSError *error;
-    NSFileManager *fileManager = [NSFileManager defaultManager];
-    NSString *documentsDirectory = [NSHomeDirectory() stringByAppendingPathComponent:@"tmp"];
-    NSString *filePath = [documentsDirectory stringByAppendingPathComponent:lastString];
-    [fileManager copyItemAtPath:moviePath toPath:filePath error:&error];
-
-    return [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:filePath];
+        NSArray* spliteArray = [moviePath componentsSeparatedByString: @"/"];
+        NSString* lastString = [spliteArray lastObject];
+        NSError *error;
+        NSFileManager *fileManager = [NSFileManager defaultManager];
+        NSString *documentsDirectory = [NSHomeDirectory() stringByAppendingPathComponent:@"tmp"];
+        NSString *filePath = [documentsDirectory stringByAppendingPathComponent:lastString];
+        [fileManager copyItemAtPath:moviePath toPath:filePath error:&error];
+        return [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:filePath];
+    } @catch (NSException *exception) {
+        NSLog(@"Camera.resultForVideo: error retrieving file path");
+        return [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@""];
+    }
 }
 
 - (void)imagePickerController:(UIImagePickerController*)picker didFinishPickingMediaWithInfo:(NSDictionary*)info
