f25e2d18f2a2b1ac4e0c13572c140ba0c21aaee6
[OSC] Siegmar Mantei <s.mantei@ndr.de>
2020-10-08 12:40:45 +0200

    Refactoring for AndroidX support.

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -37,7 +37,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import androidx.core.content.FileProvider;
 import android.util.Base64;
 
 import org.apache.cordova.BuildHelper;
