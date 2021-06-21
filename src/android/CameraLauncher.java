fea09e632fd0215f005d062158d54057b457a2a5
OJ <OJ7@users.noreply.github.com>
2021-06-21 13:03:30 -0400

    Fix launching camera when targeting SDK 30
    Fixes #673

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -302,15 +302,11 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
         intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
 
         if (this.cordova != null) {
-            // Let's check to make sure the camera is actually installed. (Legacy Nexus 7 code)
-            PackageManager mPm = this.cordova.getActivity().getPackageManager();
-            if(intent.resolveActivity(mPm) != null)
-            {
+            try {
                 this.cordova.startActivityForResult((CordovaPlugin) this, intent, (CAMERA + 1) * 16 + returnType + 1);
-            }
-            else
-            {
-                LOG.d(LOG_TAG, "Error: You don't have a default camera.  Your device may not be CTS complaint.");
+            } catch(ActivityNotFoundException e) {
+                e.printStackTrace();
+                LOG.e(LOG_TAG, "Error: You don't have a default camera.  Your device may not be CTS complaint.");
             }
         }
 //        else
