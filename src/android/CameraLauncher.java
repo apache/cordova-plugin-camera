16325102c739fe9b202ae81bc957b3c565304561
Norman Breau <norman@breautek.com>
2024-10-25 13:59:29 -0300

    fix(android): Improper serialization of image uri in save instance state (#903)

--- src/android/CameraLauncher.java
+++ src/android/CameraLauncher.java
@@ -1378,7 +1378,7 @@ public class CameraLauncher extends CordovaPlugin implements MediaScannerConnect
         }
 
         if (this.imageUri != null) {
-            state.putString(IMAGE_URI_KEY, this.imageFilePath);
+            state.putString(IMAGE_URI_KEY, this.imageUri.toString());
         }
 
         if (this.imageFilePath != null) {
