8c8707f61d0dc0a079dc078c64e3728048eb0dde
YescoSoftware <software@yesco.com>
2025-02-18 16:03:03 -0700

    Update CameraActivity.java

--- src/android/CameraActivity.java
+++ src/android/CameraActivity.java
@@ -35,7 +35,7 @@ public class CameraActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+        setContentView(R.xml.activity_camera);
 
         // Initialize views
         previewView = findViewById(R.id.preview_view);
