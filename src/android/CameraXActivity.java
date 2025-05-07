1db1cc2129cf21e41c26b28e287ddbd109a6905c
YescoSoftware <software@yesco.com>
2025-05-07 14:15:48 -0600

    Update CameraXActivity.java

--- src/android/CameraXActivity.java
+++ src/android/CameraXActivity.java
@@ -642,8 +642,7 @@ private void updateUIForOrientation(int orientation) {
                 ResolutionSelector resolutionSelector = new ResolutionSelector.Builder()
                     .setAspectRatioStrategy(AspectRatioStrategy.RATIO_4_3_FALLBACK_AUTO_STRATEGY)
                     .setResolutionStrategy(new ResolutionStrategy(
-                        ResolutionStrategy.STRATEGY_HIGHEST_AVAILABLE_QUALITY,
-                        ResolutionStrategy.FALLBACK_RULE_CLOSEST_HIGHER_THEN_LOWER))
+                        ResolutionStrategy.HIGHEST_AVAILABLE_STRATEGY))
                         .build();
 
                 // Check if device has ultra-wide camera
